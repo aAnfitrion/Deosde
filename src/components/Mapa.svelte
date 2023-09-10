61db159a14ba3e74d629cc0206bbc38e53876c59
Anfitrion <tendondevastador@hotmail.com>
2023-09-10 17:54:59 -0500

    [+] Animaciónes de movimiento

--- src/components/Mapa.svelte
+++ src/components/Mapa.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
   import { generarMapa } from "@lib/generarMapa";
   import { PERSONAJE } from "@lib/variables";
-  import { personajeMovimiento } from "@lib/manejarMapa";
+  import { personajeMovimiento } from "@lib/manejarPersonaje";
+
+  import { Motion } from "svelte-motion";
+  let i = { translateX: 0, translateY: 0 };
 
   let mapa = generarMapa();
   mapa[1].entidad = PERSONAJE.nombre;
   mapa[1].icono = PERSONAJE.icono;
 
-  function teclaPresionada(e) {
+  function teclaPresionada(e: any) {
+    const mover = (a: string, b: number, c: number) => {
+      i = { translateX: b, translateY: c };
+      setTimeout(() => {
+        personajeMovimiento(mapa, a);
+        i = { translateX: 0, translateY: 0 };
+      }, 100);
+    };
     switch (e.keyCode) {
       case 39:
-        personajeMovimiento(mapa, "derecha");
+        mover("derecha", 36, 0);
         break;
       case 37:
-        personajeMovimiento(mapa, "izquierda");
+        mover("izquierda", -36, 0);
         break;
       case 40:
-        personajeMovimiento(mapa, "abajo");
+        mover("abajo", 0, 36);
         break;
       case 38:
-        personajeMovimiento(mapa, "arriba");
+        mover("arriba", 0, -36);
         break;
     }
     mapa = mapa;
@@ -30,9 +40,16 @@
   <div class="grid grid-cols-12 gap-1">
     {#each mapa as baldosa}
       {#if baldosa.id != 0}
-        <div
-          class={`col-span-1 h-8 w-8 border border-neutral-500 ${baldosa.icono} `}
-        ></div>
+        {#if baldosa.entidad == PERSONAJE.nombre}
+          <Motion animate={i} transition={{ duration: 0.1 }} let:motion>
+            <div
+              class={`col-span-1 h-8 w-8 border border-neutral-500 ${baldosa.icono} `}
+              use:motion
+            />
+          </Motion>
+        {:else}
+          <div class={`col-span-1 h-8 w-8 `} />
+        {/if}
       {/if}
     {/each}
   </div>
