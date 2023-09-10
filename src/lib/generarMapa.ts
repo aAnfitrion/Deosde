6953d93dded075235a7e87cfe822395f7f1900c9
Anfitrion <tendondevastador@hotmail.com>
2023-09-09 20:15:53 -0500

    [*] Tamaño del json del mapa

--- src/lib/generarMapa.ts
+++ src/lib/generarMapa.ts
@@ -1,12 +1,13 @@
-type mapaD = {
+export type mapaD = {
   id: number;
   entidad: string | undefined;
+  icono: string | undefined;
 };
 
 export function generarMapa() {
   const json: madaD[] = [];
-  for (let i = 0; i < 101; i++) {
-    json.push({ id: i, entidad: undefined });
+  for (let i = 0; i < 145; i++) {
+    json.push({ id: i, entidad: undefined, icono: undefined });
   }
   return json;
 }
